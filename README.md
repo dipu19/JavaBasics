5ba76585b0bc860e7ed729e5bf44ff3bcaf67f85
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-08-22 21:36:39 +0530

    My First commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JavaBasics
+
+Interesting Problems Inside
---
+++ src/com/quantum/javabasics/MainRunner.java
@@ -0,0 +1,9 @@
+package com.quantum.javabasics;
+
+public class MainRunner {
+	
+	public static void main(String[] args) {
+		System.out.println("Repo contains interesting problems");
+	}
+
+}
