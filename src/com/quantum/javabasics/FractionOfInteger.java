d87deddabd3a992db53284fd105efd5425af5268
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-08-28 18:13:28 +0530

    Fraction of integer

---
+++ src/com/quantum/javabasics/FractionOfInteger.java
@@ -0,0 +1,17 @@
+package com.quantum.javabasics;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class FractionOfInteger {
+	public static void main(String[] args)
+	{   DecimalFormat df = new DecimalFormat("#.000000");
+       	df.setRoundingMode(RoundingMode.CEILING);
+
+		double i =2;
+		double j=5;
+		double k = i/j;
+		double s=k+(i/j);
+		System.out.println(df.format(s));
+	}
+}
