c0216796a79a8a2f1fcb77a3655b3ab1eadcf687
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-08-29 17:33:33 +0530

    counting no of time character a is present

---
+++ src/com/quantum/javabasics/RepeatedCharacter.java
@@ -0,0 +1,51 @@
+package com.quantum.javabasics;
+
+public class RepeatedCharacter {
+public static void main(String[] args)
+{
+
+{
+	String s="aca";
+	int n=10;
+	 long countForSubString=0;
+     long totalCount=0;
+     for(int i=0;i<s.length();i++)
+     {
+         if(s.charAt(i)=='a')
+         {
+             countForSubString++;
+         }
+     }
+      long divisor = n / s.length();
+
+       
+
+       totalCount += divisor * countForSubString;
+
+       
+
+       
+
+       //Determines how many characters in we will analyze towards the end of our n range
+
+       long remainder = n % s.length();
+
+       
+
+       for(int i = 0; i < remainder; i++)
+
+       {
+
+           if(s.charAt(i) == 'a')
+
+           {
+
+               totalCount++;
+
+           }
+
+       }
+System.out.println(totalCount);
+}     
+}
+}
