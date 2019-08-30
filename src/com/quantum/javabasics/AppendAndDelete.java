5828c32c5dbb51e2b73198eeafec0b5adebf19f6
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-08-30 18:07:27 +0530

    Append and delete.

---
+++ src/com/quantum/javabasics/AppendAndDelete.java
@@ -0,0 +1,35 @@
+package com.quantum.javabasics;
+
+public class AppendAndDelete {
+	public static void main(String[] args)
+	{
+		String t="hackerrank";
+		String s="hackerhappy";
+		int k=9;
+        int commonLength = 0;
+        
+        for (int i=0; i<java.lang.Math.min(s.length(),t.length());i++){
+            if (s.charAt(i)==t.charAt(i))
+                commonLength++;
+            else
+                break;
+        }
+
+        if((s.length()+t.length()-2*commonLength)>k){
+            System.out.println("No");
+        }
+
+        else if((s.length()+t.length()-2*commonLength)%2==k%2){
+            System.out.println("Yes");
+        }
+
+        else if((s.length()+t.length()-k)<0){
+            System.out.println("Yes");
+        }
+
+        else{
+            System.out.println("No");
+        }
+	}
+
+}
