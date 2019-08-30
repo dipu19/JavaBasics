78b5bd3e6ab0638e9ef6c937fcc5b0b1b89702e1
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-08-30 08:16:54 +0530

    Sequence Equation

---
+++ src/com/quantum/javabasics/SequenceEquation.java
@@ -0,0 +1,38 @@
+package com.quantum.javabasics;
+
+import java.util.ArrayList;
+
+public class SequenceEquation {
+	public static void main(String[] args)
+	{
+		int[] arr= {4,3,5,1,2};
+		ArrayList<Integer> arr1=new ArrayList<Integer>(arr.length);
+		ArrayList <Integer>value=new ArrayList<Integer>(arr.length);
+		for(int i=1;i<=arr.length;i++)
+		{
+			for(int j=0;j<arr.length;j++)
+			{
+				if(i==arr[j])
+				{
+					arr1.add(j+1);
+				}
+			}
+		}/*for(int i=0;i<arr1.size();i++)
+		{
+			System.out.println(arr1.get(i));
+		}*/for(int i=0;i<arr1.size();i++)
+		{
+			for(int j=0;j<arr1.size();j++)
+			{
+				if(arr1.get(i)==arr[j])
+				{
+					value.add(j+1);
+				}
+			}
+		}for(int i=0;i<arr1.size();i++)
+		{
+			System.out.println(value.get(i));
+		}
+	}
+
+}
