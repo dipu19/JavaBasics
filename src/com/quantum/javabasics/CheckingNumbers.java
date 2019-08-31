db784f2eec8b7a5491b589b222db0042bbe910af
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-08-31 20:46:10 +0530

    modified programme for checking number

--- src/com/quantum/javabasics/CheckingNumbers.java
+++ src/com/quantum/javabasics/CheckingNumbers.java
@@ -1,5 +1,28 @@
 package com.quantum.javabasics;
 
 public class CheckingNumbers {
+	public static void main(String[] args)
+	{
+		int number = 3;
+		if (number%2 != 0)
+		{
+			System.out.println("Weird");
+		}
+		else
+		{
+			if(number>2&number<=5)
+			{
+				System.out.println("not Weird");
+			}else if (number>6&number<=20)
+			{
+				System.out.println(" within limit");
+			}else
+			{
+				System.out.println("not within limit");
+			}
+		}
+	}
+	
+	
 
 }
