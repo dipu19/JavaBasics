cdfccecbc0103e6d2582ac726f3e6a1ad20c05bc
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-01 07:46:18 +0530

    sum of integer of array

--- src/com/quantum/javabasics/SumOfIntegers.java
+++ src/com/quantum/javabasics/SumOfIntegers.java
@@ -5,7 +5,7 @@ public class SumOfIntegers {
 	public static void main (String[] arg)
 	{   
 		int sum= 0;
-		int[] a= {1,4,4,10,25};
+		int[] a= {1,4,4,10,52};
 		int al =a.length;
 		for ( int i=0;i<al; i++)
 		{
