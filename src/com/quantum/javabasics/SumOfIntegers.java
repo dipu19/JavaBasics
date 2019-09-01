8b907632382b08033a98b57243c93953693f1514
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-01 07:40:44 +0530

    the sum integer presemt in an array.

---
+++ src/com/quantum/javabasics/SumOfIntegers.java
@@ -0,0 +1,16 @@
+package com.quantum.javabasics;
+
+public class SumOfIntegers {
+	
+	public static void main (String[] arg)
+	{   
+		int sum= 0;
+		int[] a= {1,4,4,10,25};
+		int al =a.length;
+		for ( int i=0;i<al; i++)
+		{
+			sum=sum+a[i];
+		}System.out.println(sum);	
+	}
+	
+}
