522a81ae12600c87cd6cd2b110fd76bb24831f70
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-04 17:07:58 +0530

    Prime Number

--- src/com/quantum/javabasics/PrimeNumber.java
+++ src/com/quantum/javabasics/PrimeNumber.java
@@ -1,5 +1,20 @@
 package com.quantum.javabasics;
 
 public class PrimeNumber {
-
+	public static void main(String[] args)
+	{    
+		int number = 17, i, flag=0;
+		for(i=2; i <= Math.sqrt(number);i++)
+		{
+			if(number % i == 0)
+			{
+				System.out.println("this is not a prime no");
+				flag=1;
+				break;
+		
+			}
+		}	 
+		if(flag == 0)
+			System.out.println("this is a prime no");
+	}	
 }
