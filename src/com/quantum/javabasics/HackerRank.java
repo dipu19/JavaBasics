d2d3fe553e5f8af021b7dd196dd7cd4a34215c71
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-04 11:21:39 +0530

    modified string programme

--- src/com/quantum/javabasics/HackerRank.java
+++ src/com/quantum/javabasics/HackerRank.java
@@ -1,5 +1,22 @@
 package com.quantum.javabasics;
 
-public class HackerRank {
+import java.util.ArrayList;
 
+public class HackerRank {
+	public static void main(String[] args)
+	{
+    String str = "hackerrank";
+	String s="hacckerrrank";
+    if (s.length() < str.length()) {
+        System.out.println("No");
+    }
+    int j = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (j < str.length() && s.charAt(i) == str.charAt(j)) {
+                j++;
+        }
+    }
+    System.out.println( (j == str.length() ? "YES" : "NO"));
+
+}
 }
