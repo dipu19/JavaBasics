43973fa23a3a6c08b7afe0425f27254a5b45c185
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-05 13:07:51 +0530

    finding the smallest number

---
+++ src/com/quantum/javabasics/FindingSmallestNumber.java
@@ -0,0 +1,12 @@
+package com.quantum.javabasics;
+
+import java.util.Arrays;
+
+public class FindingSmallestNumber {
+public static void main(String[] args)
+{
+	int[] arr= {50,15,48,25,30};
+	Arrays.sort(arr);
+System.out.println(arr[0]);
+}
+}
