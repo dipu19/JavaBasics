e1907f3cc099e43f21c861ca35ecf90cc2da4580
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-05 10:46:02 +0530

    circular array rotation

--- src/com/quantum/javabasics/CircularArrayRotation.java
+++ src/com/quantum/javabasics/CircularArrayRotation.java
@@ -3,12 +3,12 @@ package com.quantum.javabasics;
 public class CircularArrayRotation {
 	public static void main(String[] args)
 	{
-   int [] arr= {1,2,3,4,5};
-   int[] arr1=new int[arr.length];
-   arr1[arr1.length-1]=arr[0];
-   for(int i=0,j=1;i<arr1.length-1&j<arr.length;i++,j++)
+   int [] a= {1,2,3,4,5};
+   int[] arr1=new int[a.length];
+   arr1[arr1.length-1]=a[0];
+   for(int i=0,j=1;i<arr1.length-1&j<a.length;i++,j++)
    {
-	   arr1[i]=arr[j];;
+	   arr1[i]=a[j];;
    }
    for(int i=0;i<arr1.length;i++)
    {
