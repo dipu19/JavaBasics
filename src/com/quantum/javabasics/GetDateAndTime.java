75374b98eb8d2d7d1cf342d3eba4465e3608146d
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-07 10:51:03 +0530

    getting date and time

--- src/com/quantum/javabasics/GetDateAndTime.java
+++ src/com/quantum/javabasics/GetDateAndTime.java
@@ -1,5 +1,21 @@
 package com.quantum.javabasics;
 
-public class GetDateAndTime {
+import java.util.Date;
+import java.util.Scanner;
 
+public class GetDateAndTime {
+	public static void main(String [] args)
+	{
+		Date d1=new Date();
+		System.out.println(d1.getDate()+"/"+ (d1.getMonth()+1)+"/"+(d1.getYear()-100));
+		System.out.println("If You Want To Know The Time Then Press 1");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+	
+		if(n==1)
+		{
+			System.out.println("Thank You For Your Interest");
+		System.out.println("The Time Is "+d1.getHours()+"."+d1.getMinutes()+"."+d1.getSeconds());
+		}	
+		}
 }
