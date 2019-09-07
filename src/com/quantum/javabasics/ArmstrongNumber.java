a1689ec0e9c323bcd445a5eadee0aaacd1302164
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-07 11:40:20 +0530

    checking number is amstrong number or not

---
+++ src/com/quantum/javabasics/ArmstrongNumber.java
@@ -0,0 +1,34 @@
+package com.quantum.javabasics;
+
+public class ArmstrongNumber {
+	public static void main(String[] args)
+	{  
+		int number=154;
+		int n1=number;
+		int n2=number;
+		double sum=0;
+		
+		int count=0;
+	    while(number>0)
+	    {
+	    	number=number/10;
+	    	count=count+1;
+	    }
+	 
+		while(n1>0)
+		{
+			int d=n1%10;
+			sum=Math.pow(d, count)+sum;
+			n1=n1/10;
+		}
+		if((int)sum==n2)
+		{
+			System.out.println("It Is Armstrong Number");
+		}
+		if ((int)sum!=n2)
+		{
+			System.out.println("It Is Not Armstrong Number");
+		}
+		
+	}
+}
