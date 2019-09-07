5fcb69e333a54ee3236d0351aacac9293238a950
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-07 10:57:43 +0530

    character in string

--- src/com/quantum/javabasics/CharactersInString.java
+++ src/com/quantum/javabasics/CharactersInString.java
@@ -1,5 +1,16 @@
 package com.quantum.javabasics;
 
-public class CharactersInString {
+import java.util.Scanner;
 
+public class CharactersInString {
+	 public static void main(String[] args)
+	  {
+		  System.out.println("Enter a string");
+		  Scanner sc=new Scanner(System.in);
+		  String s1=sc.next();
+		  for(int i=0;i<s1.length();i++)
+		  {
+			  System.out.println("letter "+(i+1)+" is"+s1.charAt(i));
+		  }
+	  }
 }
