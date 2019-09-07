4b4ad2d353d481baa735238ca44366b3fb475913
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-07 10:45:11 +0530

    different character in string

--- src/com/quantum/javabasics/PrintDifferentCharacterFromString.java
+++ src/com/quantum/javabasics/PrintDifferentCharacterFromString.java
@@ -1,5 +1,20 @@
 package com.quantum.javabasics;
 
-public class PrintDifferentCharacterFromString {
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class PrintDifferentCharacterFromString {
+	public static void main(String[] args)
+	{
+      System.out.println("Enter the string");
+      Scanner sc=new Scanner(System.in);
+      String s1=sc.next();
+      Set <Character>s2=new HashSet<Character>();
+      for(int i=0;i<s1.length();i++)
+      {
+    	  s2.add(s1.charAt(i));
+      }
+      System.out.println(s2);
+}
 }
