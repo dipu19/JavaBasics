e3615c709408d53432d5e7cb6d95776124d54a7f
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-07 10:53:22 +0530

    counting number of divisor

--- src/com/quantum/javabasics/CountNoOfDivisor.java
+++ src/com/quantum/javabasics/CountNoOfDivisor.java
@@ -1,5 +1,31 @@
 package com.quantum.javabasics;
 
-public class CountNoOfDivisor {
+import java.util.Scanner;
 
+public class CountNoOfDivisor {
+	public static void main (String[] args)
+	{
+		System.out.println("Enter a number");
+		Scanner sc=new Scanner(System.in);
+		int number=sc.nextInt();
+		int count=0;
+		if(number<0)
+		{
+			System.out.println("please enter possitive integer ");
+			
+		}
+		 if(number>0)
+		{
+			for(int i=1;i<100;i++)
+			{
+				if(number%i==0)
+				{
+					count++;
+				}
+			}
+		}if(number>0)
+		 System.out.println("no of divisor is "+count);
+	}
+
+	
 }
