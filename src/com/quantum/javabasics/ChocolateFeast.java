0c6b3d2521a04b42e6a582ab03f84de26a9301e5
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-12 17:49:18 +0530

    chocolate feast

---
+++ src/com/quantum/javabasics/ChocolateFeast.java
@@ -0,0 +1,42 @@
+package com.quantum.javabasics;
+
+import java.util.Scanner;
+
+
+
+public class ChocolateFeast {
+	public int chocolateFeast(int n,int c,int m)
+	{
+		//n=total amount bobby has.
+		//c=cost of the chocolate.
+		//m=number wrapper need for a chocolate.
+        int numberOfChocolates = n / c;
+        int totalWrappers = numberOfChocolates;
+        int freeChocolate = 0;
+        while (m <= totalWrappers) {
+            int wrapperTogive=totalWrappers-(totalWrappers%m) ;
+
+            freeChocolate = totalWrappers / m;
+
+            totalWrappers = (totalWrappers - wrapperTogive) + freeChocolate;
+
+            numberOfChocolates=numberOfChocolates + freeChocolate;
+
+        }
+
+
+		return numberOfChocolates;
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("Enter the money you have");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("Enter the price of the chocolate");
+		int c=sc.nextInt();
+		System.out.println("Enter the number of wrapper need to get a chocolate ");
+		int m=sc.nextInt();
+		ChocolateFeast sa=new ChocolateFeast();
+		System.out.println("number of chocolate you can get :"+sa.chocolateFeast(n, c, m));
+	}
+}
