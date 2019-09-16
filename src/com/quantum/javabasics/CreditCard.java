5c03bca5082277133318d2583f0a6828e3c19cc8
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-16 18:41:49 +0530

    Project of credit card

---
+++ src/com/quantum/javabasics/CreditCard.java
@@ -0,0 +1,46 @@
+package com.quantum.javabasics;
+
+import java.util.Scanner;
+
+
+	public class CreditCard {
+		public static void main(String[] args)
+		{
+			 System.out.println("Enter pin");
+			 Scanner reader=new Scanner(System.in);
+			 int pin=reader.nextInt();
+			 final int pincode=7080;
+			 final int expYear=2025;
+			 final int expMonth=8;
+			 int balance=450200;
+			 if(pin==pincode)
+			 {
+				 System.out.println("Enter exp year");
+				 Scanner t1=new Scanner(System.in);
+				 int year=t1.nextInt();
+				 if(year==expYear)
+				 {
+					 System.out.println("Enter exp month");
+					 Scanner t2=new Scanner(System.in);
+					  int month=t2.nextInt();
+					 if(month==expMonth)
+					 {
+						 System.out.println("Your Balance is "+balance);
+					 }
+				     else
+				     {
+				    	 System.out.println("You have entered wrong month.");
+				     }
+					 }
+				 else
+				 {
+					 System.out.println("You have entered wrong year.");
+				 }
+			 }
+			 else 
+			 {
+				 System.out.println("You have entered wrong pin.");
+			 }
+			 System.out.println("Thank you");
+		}
+}
