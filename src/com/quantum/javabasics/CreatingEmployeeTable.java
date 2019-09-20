ca11444957b67618dcbc5df047e9d2974524557d
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-20 18:58:13 +0530

    Employee table

--- src/com/quantum/javabasics/CreatingEmployeeTable.java
+++ src/com/quantum/javabasics/CreatingEmployeeTable.java
@@ -1,5 +1,25 @@
 package com.quantum.javabasics;
 
-public class CreatingEmployeeTable {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
+public class CreatingEmployeeTable {
+	public static String creationTable="CREATE TABLE EmpTable("+"NAME VARCHAR(20))";
+	
+	  public static void main (String[] args)
+	  {
+		  try
+		  {
+		  Class.forName("com.mysql.jdbc.Driver");
+		  Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+		  Statement st=conn.createStatement();
+		  st.executeUpdate(creationTable);
+		  System.out.println("Connected");
+		  }
+		  catch(Exception e)
+		  {
+			 e.printStackTrace();
+		  }
+	  }
 }
