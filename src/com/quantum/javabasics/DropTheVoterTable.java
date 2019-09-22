dacff3e7d0f1f1098ec1394764335866d17c0fbb
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-22 15:00:18 +0530

    Drop the table from database.

---
+++ src/com/quantum/javabasics/DropTheVoterTable.java
@@ -0,0 +1,20 @@
+package com.quantum.javabasics;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public class DropTheVoterTable {
+	public static void main(String [] args) throws Exception
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
+		Statement st=conn.createStatement();
+		PreparedStatement ps=conn.prepareStatement("INSERT INTO voter(?,?)");
+		ps.setString(2, "Jay");
+		//st.execute("Insert into Voter values('Jay',48)");
+		st.execute("drop table voter");
+		System.out.println("Inserted");
+	}
+}
