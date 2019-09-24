06a0ee36e4fcf0109c3c620f24e95172f51f1899
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-24 19:23:41 +0530

    Modified Creating Table.

--- src/com/quantum/javabasics/Cricket_Team.java
+++ src/com/quantum/javabasics/Cricket_Team.java
@@ -6,11 +6,10 @@ public class Cricket_Team {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
 		Statement st=conn.createStatement();
-		st.executeUpdate("CREATE TABLE Crick_Table (P_name VARCHAR(30),P_age int,P_id int)");
-		st.executeUpdate("drop table Crick_Table");
+//		st.executeUpdate("drop table Crick_Table");
 		
 		  PreparedStatement ps=conn.prepareStatement("Insert into users(?,?,?) ");
-		  ps.setString(1, "Dipuu"); ps.setInt(1,15); ps.setInt(2,12);
+		  ps.setString(1, "Dipuu"); ps.setInt(1,15); ps.setInt(1,12);
 		 
 		System.out.println("Done");
 }
