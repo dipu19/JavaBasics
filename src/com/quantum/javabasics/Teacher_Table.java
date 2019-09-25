890499d6c0d0f48f520e9a7c045c4e89aec1e3b2
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-09-25 15:35:31 +0530

    creating teacher table

---
+++ src/com/quantum/javabasics/Teacher_Table.java
@@ -0,0 +1,27 @@
+package com.quantum.javabasics;
+import java.sql.*;
+public class Teacher_Table {
+	public static void main(String[] args)
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
+			Statement st=con.createStatement();
+			PreparedStatement ps=con.prepareStatement("insert into teacherTable(?,?)");
+			//st.executeUpdate("CREATE TABLE teacherTable(Tname VARCHAR(20),Tid Int)");
+			st.execute("insert into teacherTable values('Dipu',1455)");
+			ps.setString(1,"Ratan");  
+			  
+			int i=ps.executeUpdate();  
+			System.out.println(i+" records inserted");  
+			  
+			//st.execute("insert into teacherTable values('Dipu',1455)");
+			System.out.println("Done");
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception ="+e );
+		}
+	}
+}
