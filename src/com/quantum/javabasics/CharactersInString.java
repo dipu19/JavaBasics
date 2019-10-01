603a16a34bf51e77cd0d317e073405cccf1cd916
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-10-01 21:51:27 +0530

    Character String

--- src/com/quantum/javabasics/CharactersInString.java
+++ src/com/quantum/javabasics/CharactersInString.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CharactersInString {
 	 public static void main(String[] args)
 	  {
-		  System.out.println("Enter a string");
+		  System.out.println("Enter the string");
 		  Scanner sc=new Scanner(System.in);
 		  String s1=sc.next();
 		  for(int i=0;i<s1.length();i++)
