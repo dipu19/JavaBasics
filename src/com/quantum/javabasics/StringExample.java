7c90b5cdceaaf8f148f0c7247e4940b3802667ec
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-10-01 21:49:14 +0530

    Modified String Example

--- src/com/quantum/javabasics/StringExample.java
+++ src/com/quantum/javabasics/StringExample.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class StringExample {
  public static void main(String[] args)
  {
-	 System.out.println("Enter the String");
-	 Scanner sc= new Scanner(System.in);
-	 String st=sc.toString();
-	 System.out.println("Enter the 2nd String");
-	 String st1=sc.toString();
-	 String st3=st.concat(st1);
-	 System.out.println(st3);
-	 String st2="ram";
-	 System.out.println(st.concat(st2));
+		
+		  System.out.println("Enter the String"); Scanner sc= new Scanner(System.in);
+		  String st=sc.toString(); System.out.println("Enter the 2nd String"); String
+		  st1=sc.toString(); String st3=st.concat(st1); System.out.println(st3); String
+		  st2="ram"; System.out.println(st.concat(st2));
+		 
  }
 }
