a6bbc810e79acd8978aeb2e65681cbca279b04a2
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-10-06 12:20:35 +0530

    Default vlaue given by jvm

---
+++ src/com/quantum/javabasics/Default_Value_Of_Datatype.java
@@ -0,0 +1,18 @@
+package com.quantum.javabasics;
+
+public class Default_Value_Of_Datatype {
+	char ch;
+	int no;
+	String s;
+	boolean b;
+	double d;
+	public static void main(String[] args)
+	{
+		Default_Value_Of_Datatype d=new Default_Value_Of_Datatype();
+		System.out.println("Default vlaue of char "+ d.ch);
+		System.out.println("Default vlaue of int "+ d.no);
+		System.out.println("Default vlaue of String "+ d.s);
+		System.out.println("Default vlaue of boolean"+ d.b);
+		System.out.println("Default vlaue of double "+ d.d);
+	}
+}
