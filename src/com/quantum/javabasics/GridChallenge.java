961366cc37c14aeeecd97b6621f7135cbc727be4
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-10-15 12:51:08 +0530

    grid challange problem

---
+++ src/com/quantum/javabasics/GridChallenge.java
@@ -0,0 +1,13 @@
+package com.quantum.javabasics;
+import java.util.*;
+public class GridChallenge {
+	public static void main(String[] args)
+	{
+		char[] arr= {'a','z','b','c'};
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
