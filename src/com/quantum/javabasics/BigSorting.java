85d30711e46e5f9f1d8a2e2954e337c8b5beaac6
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-11-03 11:30:42 +0530

    Big sorting

--- src/com/quantum/javabasics/BigSorting.java
+++ src/com/quantum/javabasics/BigSorting.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class BigSorting {
 public static void main(String[] args)
 {
-	System.out.println("Enter the value of n");
+	System.out.println("Enter the value of n ");
 	Scanner sc=new Scanner(System.in);
 	int n=sc.nextInt();
- System.out.println("Enter the value to array");
- String [] unsorted=new String [n];
+    System.out.println("Enter the value to array");
+    String [] unsorted=new String [n];
 
  for(int i=0;i<n;i++)
  {
