81e09fcbdf632f7979ae923fc0e22712a32a2a5c
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-11-05 23:24:38 +0530

    Casear problem

--- src/com/quantum/javabasics/CasearCipher.java
+++ src/com/quantum/javabasics/CasearCipher.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class CasearCipher {
  public static void main(String[] args)
  {
-	 System.out.println("Enter a string");
+	 System.out.println("Enter the string");
 	 Scanner sc=new Scanner(System.in);
 	 String input=sc.next();
 	 ArrayList <Character>ar=new ArrayList<Character>(input.length());
