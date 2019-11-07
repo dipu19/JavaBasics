8e81db9d17770c4e00c61db879b7c7a4ca49fa65
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-11-07 11:39:35 +0530

    closest number

--- src/com/quantum/javabasics/ClosestNumber.java
+++ src/com/quantum/javabasics/ClosestNumber.java
@@ -4,26 +4,40 @@ import java.util.*;
 public class ClosestNumber {
 	public static void main(String[] args)
 	{
-		System.out.println("Enter the size of array");
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of array");
 		int n=sc.nextInt();
-		System.out.println("Enter elements to array");
-		int[] arr=new int[n];
-		ArrayList<Integer> arr1=new ArrayList<Integer>(20);
-		for(int i=0;i<n;i++)
-		{
-			arr[i]=sc.nextInt();
-		}
-		Arrays.sort(arr);
-		for(int i=0,j=1;i<n&j<n;i++,j++)
-		{
-			
-				if(arr[j]-arr[i]==1)
-				{
-					arr1.add(arr[i]);
-					arr1.add(arr[j]);
-				}
-			
-		}System.out.println(arr1);
+		int [] a=new int[n];
+		ArrayList <Integer>ar=new ArrayList<Integer>();
+		int []ar1=new int[ar.size()];
+		System.out.println("Enter values to array");
+		 for(int i=0;i<n;i++)
+	        {
+	            a[i] = sc.nextInt();
+	        }
+	        Arrays.sort(a);
+	        int d =a[1]-a[0],id=0;
+	        for(int i=1;i<n;i++)
+	        {
+	            if(d>Math.abs(a[i]-a[i-1]))
+	            {
+	                d = Math.abs(a[i]-a[i-1]);
+	                id = i-1;
+	            }
+	        }
+	        for(int i=id;i<n-1;i++)
+	        {
+	            if(d==Math.abs(a[i]-a[i+1]))
+	              ar.add(a[i]);
+	            ar.add(a[i+1]);
+	        }
+	        for(int i=0;i<ar1.length;i++)
+	        {
+	        	ar1[i]=ar.get(i);
+	        }
+	        for(int i=0;i<ar1.length;i++)
+	        {
+	        	System.out.println(ar1[i]);
+	        }System.out.println(ar);
 	}
 }
