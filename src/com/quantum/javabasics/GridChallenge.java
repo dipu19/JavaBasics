731f5462ca23c174d99f83d1be6fa56af301777c
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-11-07 11:38:07 +0530

    Grid challenge

--- src/com/quantum/javabasics/GridChallenge.java
+++ src/com/quantum/javabasics/GridChallenge.java
@@ -1,13 +1,48 @@
 package com.quantum.javabasics;
+
 import java.util.*;
 public class GridChallenge {
+
 	public static void main(String[] args)
 	{
-		char[] arr= {'a','z','b','c'};
-		Arrays.sort(arr);
-		for(int i=0;i<arr.length;i++)
+		System.out.println("enter value of r");
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		System.out.println("enter value of c");
+		int c=sc.nextInt();
+		System.out.println("enter values to array");
+		char [][] arr=new char[r][c];
+		for(int i=0;i<r;i++)
 		{
-			System.out.println(arr[i]);
+			for(int j=0;j<c;j++)
+			{
+				arr[i][j]=(char)sc.nextInt();
+			}
+		}
+		int count=0;
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0,k=j+1;j<c&k<c;j++,k++)
+			{
+				if((int)arr[i][j]<arr[i][k])
+				{
+					count++;
+				}
+			}
+			
+		}
+		
+		  for(int i=0;i<c;i++) { for(int j=0,k=j+1;j<r&k<r;j++,k++) {
+		  if((int)arr[j][i]<(int)arr[k][i]) { count++; } } }
+		 
+		System.out.println(count);
+		if(count==r*c)
+		{
+			System.out.println("YES");
+		}
+		else 
+		{
+			System.out.println("NO");
 		}
 	}
 }
