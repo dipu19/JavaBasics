7287c00ebbb4f10967d550ae4b601b7f6dfe5f30
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-12-23 14:38:20 +0530

    Bubble Short

---
+++ src/com/quantum/javabasics/BuubleSort.java
@@ -0,0 +1,32 @@
+package com.quantum.javabasics;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BuubleSort {
+	public static void main(String[] args)
+	{
+		System.out.println("Enter the size of array");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("Enter elements to array");
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		//comparing two adjacent element
+		for(int i=0;i<n-1;i++)
+		{
+			for(int j=0;j<n-1;j++)
+			{
+				if (arr[j]>arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}System.out.println(Arrays.toString(arr));
+	}
+}
