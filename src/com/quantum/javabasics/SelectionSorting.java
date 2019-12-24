58beca8e690c47768838a6e452a2c2ce8256ba93
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-12-24 16:18:45 +0530

    Selection Sorting

---
+++ src/com/quantum/javabasics/SelectionSorting.java
@@ -0,0 +1,37 @@
+package com.quantum.javabasics;
+
+import java.util.Scanner;
+
+//Selection Sorting
+public class SelectionSorting {
+	public static void main (String[] args)
+	{
+		System.out.println("Enter the size of array");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("Enter elements to array");
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		
+		}
+		 for (int i = 0 ;i< n-1; i++){
+	          int min = i;
+
+	         for (int j = i+1; j<n; j++){
+	            if (arr[j] < arr[min]){
+	            min = j;
+	            }
+	         }
+	         int temp = arr[min];
+	         arr[min] = arr[i];
+	         arr[i] = temp;
+	      }
+		 for(int i=0;i<n;i++)
+		 {
+			 System.out.println(" "+arr[i]);
+		 }
+ 
+	}
+}
