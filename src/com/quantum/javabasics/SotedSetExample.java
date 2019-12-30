30ee73c771e65cae495c883cdff847c854176520
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2019-12-30 12:40:41 +0530

    Sorted Set example

---
+++ src/com/quantum/javabasics/SotedSetExample.java
@@ -0,0 +1,18 @@
+package com.quantum.javabasics;
+
+import java.util.*;
+
+//Example on Sorted map 
+public class SotedSetExample {
+	public static void main(String[] args)
+	{
+		SortedMap <Integer,String>sm=new TreeMap<Integer,String>();
+		sm.put(1,"Dipu");
+		sm.put(0,"Sipu");
+		sm.put(2,"Tossali");
+		sm.put(3,"Tapash");
+		System.out.println(sm);
+		System.out.println(sm.lastKey());
+				
+	}
+}
