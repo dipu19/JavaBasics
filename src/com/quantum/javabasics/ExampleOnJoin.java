116b3fe27972a6f3d256a6a0efa5196612a87976
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2020-01-03 11:26:40 +0530

    Exqmple on join

---
+++ src/com/quantum/javabasics/ExampleOnJoin.java
@@ -0,0 +1,36 @@
+package com.quantum.javabasics;
+	class Right extends Thread
+	{
+		public void run()
+		{
+			for(int i=0;i<5;i++)
+			{
+				System.out.println("jai");
+			
+			  try { Thread.sleep(2000); } catch(Exception e) { System.out.println("shree");
+			  }
+			 
+				
+			}
+		}
+	}
+	public class ExampleOnJoin 
+	{
+		public static void main(String[] args) throws Exception
+		{
+			Right r=new Right();
+			r.start();
+			r.join();
+			for(int i=0;i<4;i++)
+			{
+				System.out.println("Jagannath");
+				try {
+					Thread.sleep(2000);
+				}
+				catch(Exception e)
+				{
+					System.out.println("Ram");
+				}
+			}
+		}
+    }
