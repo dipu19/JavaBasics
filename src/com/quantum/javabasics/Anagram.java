13fcd037c6d5a5fe7dbbe81512d8be03af05ff9d
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2020-01-14 11:44:03 +0530

    String Anagram

--- src/com/quantum/javabasics/Anagram.java
+++ src/com/quantum/javabasics/Anagram.java
@@ -1,35 +1,58 @@
 package com.quantum.javabasics;
 import java.util.*;
 public class Anagram {
-
-	public static void main(String[] args) {
-		 
-		        Scanner sc = new Scanner(System.in);
-		        System.out.println("Enter string length");
-		        int t = sc.nextInt();
-		        System.out.println("Enter String");
-		        while (t-->0){
-		            String str = sc.next();
-		            int len = str.length(), count = 0;
-		            if (len%2!=0){
-		                System.out.println(-1);
-		                continue;
-		            }
-		            String s1 = str.substring(0,len/2);
-		            String s2 = str.substring(len/2,len);
-		            char[] s1Chars = s1.toCharArray();
-		            for (char c : s1Chars){
-		                int index = s2.indexOf(c);
-		                if (index == -1){
-		                    count++;
-		                } else {
-		                    s2 = s2.substring(0,index)+s2.substring(index+1);
-		                }
-		            }
-		            System.out.println(count);
-		        }
-		    }
-
+	
+	public void anagram(String s1,String s2)
+	{
+		String res="not anagram";
+		int count=0;
+		char[] al1 =new char[s1.length()];
+		char[] al2 =new char[s2.length()];
+		if(s1.length()!=s2.length())
+		{
+			System.out.println("Not anagram");
+		}
+		else
+		{
+		for(int i=0;i<s1.length();i++)
+		{
+			al1[i]=s1.charAt(i);
+			al2[i]=s2.charAt(i);
+			} /*
+				 * System.out.println(al1); System.out.println(al2);
+				 */
+		Arrays.sort(al1);
+		Arrays.sort(al2);
+			/*
+			 * for(int i=0;i<al1.size();i++) { for(int j=1;j<al1.size();j++) {
+			 * if((int)al1.get(i)>(int)al1.get(j)) { char temp=al1.get(i); al1.add(i,
+			 * al1.get(j)); al1.add(j,temp); } } } for(int i=0;i<al2.size();i++) { for(int
+			 * j=1;j<al2.size();j++) { if((int)al2.get(i)>(int)al2.get(j)) { char
+			 * temp=al2.get(i); al2.add(i, al2.get(j)); al2.add(j,temp); } } }
+			 */
+		for(int i=0;i<al1.length;i++)
+		{
+			if(al1[i]==al2[i])
+			{
+				count++;
+			}
+		}
+		if (count==al1.length)
+		{
+			System.out.println("anagram");
+		}
+		else
+		{
+			System.out.println("not anagram");
+		}
+		}
+		
+		
+		
 	}
-
+	public static void main(String[] args) {
+		Anagram ag=new Anagram();
+     ag.anagram("paap", "appaa");
+	}
+}
 
