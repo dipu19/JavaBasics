7284f8b596351ab3df392bedac27837d16535f5e
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2020-01-14 19:26:01 +0530

    Printing Pattern

---
+++ src/com/quantum/javabasics/PrintingPattern.java
@@ -0,0 +1,16 @@
+package com.quantum.javabasics;
+
+public class PrintingPattern {
+	
+	public static void main(String[] args)
+	{ for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5 - i; j++) {
+            System.out.print(" ");
+        }
+        for (int k = 0; k <= i; k++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+	}
+}
