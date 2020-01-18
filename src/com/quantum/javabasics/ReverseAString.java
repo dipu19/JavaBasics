76de06433db9de56e81313a3abf92c20acf40dce
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2020-01-18 08:58:42 +0530

    Reverse a string

--- src/com/quantum/javabasics/ReverseAString.java
+++ src/com/quantum/javabasics/ReverseAString.java
@@ -8,9 +8,9 @@ public class ReverseAString {
 		String s2="";
 		for(int i=s1.length()-1;i>=0;i--)
 		{
-			System.out.print(s1.charAt(i));
-			//System.out.println(i);
+			s2=s2+s1.charAt(i);
+			
 		}
-		
+		System.out.println(s2);
 	}
 }
