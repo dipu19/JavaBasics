4fa0635bc04855e9b54dfb40a1c05dcee0d10f3a
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2020-01-18 08:55:14 +0530

    removing duplicate element

--- src/com/quantum/javabasics/RemovingDuplicateElement.java
+++ src/com/quantum/javabasics/RemovingDuplicateElement.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class RemovingDuplicateElement {
 	public static void main(String[] args)
 	{
-		int[] arr= {4,5,5,6,7,7,8,9,9,10,10};
+		int[] arr= {4,5,6,7,7,8,9,9,10,10};
 		 ArrayList arr1=new ArrayList(100);
 		 arr1.add(arr[0]);
 		 for(int i=0,j=1;i<arr.length&j<arr.length;i++,j++)
