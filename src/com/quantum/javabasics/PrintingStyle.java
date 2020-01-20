0e226332620eaf54de50e7d050b61b6607a3a7bc
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2020-01-20 15:11:49 +0530

    printing style

---
+++ src/com/quantum/javabasics/PrintingStyle.java
@@ -0,0 +1,15 @@
+package com.quantum.javabasics;
+
+public class PrintingStyle {
+
+	public static void main(String[] args)
+	{
+		for(int i=0;i<5;i++)
+		{
+			for(int j=5-i;j>0;j--)
+			{
+				System.out.print("*"+" ");
+			}System.out.println( );
+		}
+	}
+}
