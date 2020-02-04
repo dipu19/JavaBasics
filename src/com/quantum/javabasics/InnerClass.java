c38c695842a82b8995712b393af6aeeb840a9bec
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2020-02-04 20:16:54 +0530

    inner class

--- src/com/quantum/javabasics/InnerClass.java
+++ src/com/quantum/javabasics/InnerClass.java
@@ -6,20 +6,15 @@ package com.quantum.javabasics;
 class Outer_Demo {
    int num;
    
-  
    private class Inner_Demo {
       public void print() {
          System.out.println("This is an inner class");
       }
-   }
-   
-  
-   void display_Inner() {
+   }   void display_Inner() {
       Inner_Demo inner = new Inner_Demo();
       inner.print();
    }
 }
-   
 public class InnerClass {
 
    public static void main(String args[]) {
