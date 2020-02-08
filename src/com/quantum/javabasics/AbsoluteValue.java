53dad3f4c7273b703cb7b2ee89962db901029a7d
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2020-02-08 20:27:41 +0530

    Absolute  value

--- src/com/quantum/javabasics/AbsoluteValue.java
+++ src/com/quantum/javabasics/AbsoluteValue.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class AbsoluteValue {
 	public static void main(String[] args )
 	{
-		System.out.println("Enter the integer");
+		System.out.println("Enter integer");
 		Scanner sc=new Scanner(System.in);
 		int number=sc.nextInt();
 		System.out.println(Math.abs(number));
