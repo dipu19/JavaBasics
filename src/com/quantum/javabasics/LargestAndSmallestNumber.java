05ef8262235a71bfce6cf93466ea8cbc95079d44
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2020-02-11 17:07:18 +0530

    Largest and Smallest number of array

---
+++ src/com/quantum/javabasics/LargestAndSmallestNumber.java
@@ -0,0 +1,35 @@
+package com.quantum.javabasics;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LargestAndSmallestNumber {
+	//largest number
+	public void large(int[] arr)
+	{
+		Arrays.sort(arr);
+		System.out.println("Largest element of array is "+arr[arr.length-1]);
+	}
+	//smallest number
+	public void small(int[] arr)
+	{
+		Arrays.sort(arr);
+		System.out.println("Smallest element of array is "+arr[0]);
+	}
+	public static void main(String[] args)
+	{
+		LargestAndSmallestNumber obj=new LargestAndSmallestNumber();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of array");
+		int size=sc.nextInt();
+		int[] arr=new int[size];
+		System.out.println("Enter element to array");
+		for(int i=0;i<size;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		obj.large(arr);
+		System.out.println();
+		obj.small(arr);
+	}
+}
