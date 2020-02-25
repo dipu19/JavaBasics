7001d78c3cece803ed04b9f672bca34f268f8180
Swarna Sukanta Pradhan <swarnasukanta001@gmail.com>
2020-02-25 14:21:38 +0530

    People liked the add

--- src/com/quantum/javabasics/AdLikedByPeople.java
+++ src/com/quantum/javabasics/AdLikedByPeople.java
@@ -6,16 +6,16 @@ public class AdLikedByPeople {
 		System.out.println("Enter till the days you want to calculate ");
 		Scanner d=new Scanner(System.in);
 		int days=d.nextInt();
-		int share=5;//1st day 5 people share the ad.
-		int like=0;//from shared people (share/2) people like the ad.
+		int share=5;
+		int like=0;
 		int total=0;
 		for(int i=1;i<=days;i++)
 		{
 			like=share/2;
-			share=like*3;//from 2nd day every people who like the ad they share to 3 people.
+			share=like*3;
 			total=total+like;
 		}
-		System.out.println("Total person liked the ad ="+total);
+		System.out.println("Total person liked the ad "+total);
 	}
 
 }
