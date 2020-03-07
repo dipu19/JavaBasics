46fa69389363efe96b1dcf10b51db4f92337261f
Dipu <swarnasukanta001@gmail.com>
2020-03-07 12:28:58 +0530

    Exception practice

--- src/com/quantum/javabasics/ExceptionPractice.java
+++ src/com/quantum/javabasics/ExceptionPractice.java
@@ -2,21 +2,21 @@ package com.quantum.javabasics;
 
 
 
-class TooYoung extends RuntimeException
+class Young extends RuntimeException
 {
-	public TooYoung(String s) {
+	public Young(String s) {
 		super(s);
 }
 
 
 }
 public class ExceptionPractice {
-		public static void main(String[] args)throws ArithmeticException
+		public static void main(String[] args)throws Exception
 	{
 		int age=16;
 		if(age<18)
 		{
-			throw new TooYoung("Your age is not eligible to vote");
+			throw new Young("Your age is not eligible to vote");
 		}
 	}
 }
