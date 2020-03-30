21a812519e64d57767536d8ae12cda4845ff4b68
Dipu <swarnasukanta001@gmail.com>
2020-03-30 11:16:23 +0530

    Conversion of binary to decimal

---
+++ src/com/quantum/javabasics/ConversionOfBinaryToDecimal.java
@@ -0,0 +1,31 @@
+package com.quantum.javabasics;
+
+import java.util.ArrayList;
+
+public class ConversionOfBinaryToDecimal {
+
+	public void conversion(int  number)
+	{
+		ArrayList <Integer>ar =new ArrayList<Integer>();
+		double  result = 0;
+		while(number >0)
+		{
+			ar.add(number%10);
+			number = number/10;
+			
+		
+		}
+		for(int  i= 0;i<ar.size();i++)
+		{
+			result = result+(Math.pow(2,i)*ar.get(i));
+			
+		}
+		
+		System.out.println((int)result);
+	}
+	public static void main(String[] args)
+	{
+		ConversionOfBinaryToDecimal object = new ConversionOfBinaryToDecimal();
+		object.conversion(111111);
+	}
+}
