836eec5c1fe85696d2655f70bbc1d75df0503f4f
Dipu <swarnasukanta001@gmail.com>
2020-05-30 11:55:52 +0530

    Out put problem

---
+++ src/com/quantum/javabasics/OutPut.java
@@ -0,0 +1,10 @@
+package com.quantum.javabasics;
+
+public class OutPut {
+
+	public static void main(String[] args)
+	{
+	 //uoodSystem.out.println("Comment Executed");
+	}
+}
+//it will give invalid unicode
