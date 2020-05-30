e1db280328cf44591408c9d9e6b83f86bbcdf699
Dipu <swarnasukanta001@gmail.com>
2020-05-30 11:52:57 +0530

    Getting out put

---
+++ src/com/quantum/javabasics/OutPutOne.java
@@ -0,0 +1,10 @@
+package com.quantum.javabasics;
+
+public class OutPutOne {
+
+	public static void main(String[] args)
+	{
+		int i = 10 + + 11 - - 12 + + 13 - - 14 + + 15;
+		System.out.println(i);
+	}
+}
