eb85b0e010fe38f90ead71d2ca2bb6655cf4301f
Dipu <swarnasukanta001@gmail.com>
2020-06-16 09:30:24 +0530

    Overloading in java

--- src/com/quantum/javabasics/OverloadingExample.java
+++ src/com/quantum/javabasics/OverloadingExample.java
@@ -14,10 +14,23 @@ package com.quantum.javabasics;
 		 
 		 System.out.println(a+""+b);
 	 }
+	 
+	 public void add(long a)
+	 {
+		 System.out.println(a + 10);
+	 }
+	 public void add (String s)
+	 {
+		 System.out.println(s+10);
+	 }
+	 
 	 public static void main(String[] args)
 	 {
 		 OverloadingExample oe=new OverloadingExample();
 		 oe.add("Jay","Jagannath");
 		 oe.add(45, 55);
+		 
+		 int l= 15;
+		 oe.add(l);
 	 }
  }
