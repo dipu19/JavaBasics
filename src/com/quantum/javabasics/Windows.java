db0a81966ca6ad70770b3ffa1ce84d8a6f5294b9
Dipu <swarnasukanta001@gmail.com>
2020-06-28 22:03:55 +0530

    Windows class for factory design pattern

---
+++ src/com/quantum/javabasics/Windows.java
@@ -0,0 +1,10 @@
+package com.quantum.javabasics;
+
+public class Windows implements OperatingSysytem{
+
+	@Override
+	public void specification() {
+		System.out.println("Welcome to windows family");
+	}
+
+}
