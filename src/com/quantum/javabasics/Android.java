09cc918ed6dadf71b119d5be81b67040d4392e18
Dipu <swarnasukanta001@gmail.com>
2020-06-28 22:02:04 +0530

    Android class for factory design pattern

---
+++ src/com/quantum/javabasics/Android.java
@@ -0,0 +1,11 @@
+package com.quantum.javabasics;
+
+public class Android implements OperatingSysytem{
+
+	@Override
+	public void specification() {
+		System.out.println("Welcome to android family");
+	}
+
+	
+}
