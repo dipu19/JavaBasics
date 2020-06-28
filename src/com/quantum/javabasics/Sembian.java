f586278251eac931d7b711e007cbe14777dcd3ae
Dipu <swarnasukanta001@gmail.com>
2020-06-28 22:05:04 +0530

    Sembian class for  factory design pattern

---
+++ src/com/quantum/javabasics/Sembian.java
@@ -0,0 +1,11 @@
+package com.quantum.javabasics;
+
+public class Sembian implements OperatingSysytem{
+
+	@Override
+	public void specification() {
+		System.out.println("Welcome to sembian family");
+		
+	}
+
+}
