ce7baafa9edb8cd2b511a8d36c99ccd3b402be56
dipu19 <swarnasukanta001@gmail.com>
2020-07-19 20:56:59 +0530

    Main method in interface

---
+++ src/com/quantum/javabasics/MainMethodInInterface.java
@@ -0,0 +1,9 @@
+package com.quantum.javabasics;
+
+public interface MainMethodInInterface {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello i am main method in interface");
+	}
+}
